783c8e172c4533042077f37dcb336035310074d2
Test <Test@test.com>
2021-09-13 23:24:47 -0400

    Added assignment code

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,47 @@
 package org.example;
 
-/**
- * Hello world!
- *
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 21 Solution
+ *  Copyright 2021 Tyler Coleman
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter the number of the month: ");
+        int monthChoice = in.nextInt();
+        String monthString;
+        switch (monthChoice) {
+            case 1: monthString = "January";
+                break;
+            case 2:  monthString = "February";
+                break;
+            case 3:  monthString = "March";
+                break;
+            case 4:  monthString = "April";
+                break;
+            case 5:  monthString = "May";
+                break;
+            case 6:  monthString = "June";
+                break;
+            case 7:  monthString = "July";
+                break;
+            case 8:  monthString = "August";
+                break;
+            case 9:  monthString = "September";
+                break;
+            case 10: monthString = "October";
+                break;
+            case 11: monthString = "November";
+                break;
+            case 12: monthString = "December";
+                break;
+            default: monthString = "That's not a valid month number.";
+                break;
+        }
+        System.out.printf("The name of the month is %s.", monthString);
     }
 }
